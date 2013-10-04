6cc1715a2b1ac00ee04c4a5a0aaf035c3ea30ca0
Jesper Sindahl Nielsen <jesper.sindahl.nielsen@gmail.com>
2013-10-04 20:15:46 +0200

    parsing about half the language now

--- lexer.py
+++ lexer.py
@@ -21,6 +21,33 @@ TK_VAL = 17
 TK_IN = 18
 TK_NAME = 19
 TK_COLON = 20
+TK_INT = 21
+TK_TEXT = 22
+TK_ZERO = 23
+TK_ONE = 24
+TK_STDBOOL = 25
+TK_STDINT = 26
+TK_STDTEXT = 27
+TK_COMMA = 28
+TK_FUNC = 29
+TK_END = 30
+TK_RIGHTARROW = 31
+TK_TYPE_BOOL = 32
+TK_TYPE_INT = 33
+TK_TYPE_TEXT = 34
+TK_TYPE_ATOM = 35
+TK_TYPE_TUP = 36
+TK_TYPE_REL = 37
+TK_TYPE_FUNC = 38
+TK_TYPE_ANY = 39
+TK_EQUAL = 40
+TK_ASSIGN = 41
+TK_PLUS = 42
+TK_MUL = 43
+TK_DIV = 44
+TK_MOD = 45
+TK_SEMICOLON = 46
+TK_CONCAT = 47
 
 tokenMap = sorted(
     [(TK_TUP, "tup"), 
@@ -28,14 +55,43 @@ tokenMap = sorted(
      (TK_AT, "@"),
      (TK_TRUE, "true"),
      (TK_FALSE, "false"), 
+     (TK_SHARP, "#"),
      (TK_LPAREN, "("), 
-     (TK_RPAREN, "("), 
+     (TK_RPAREN, ")"), 
      (TK_NOT, "not"),
+     (TK_MINUS, "-"),
+     (TK_OR, "or"),
+     (TK_AND, "and"),
      (TK_BLOCKSTART, "(+"),
      (TK_BLOCKEND, "+)"), 
      (TK_VAL, "val"),
      (TK_IN, "in"),
      (TK_COLON, ":"),
+     (TK_ZERO, "zero"),
+     (TK_ONE, "one"),
+     (TK_STDBOOL, "?-Bool"),
+     (TK_STDINT, "?-Int"),
+     (TK_STDTEXT, "?-Text"),
+     (TK_COMMA, ","),
+     (TK_FUNC, "func"),
+     (TK_END, "end"),
+     (TK_RIGHTARROW, "->"),
+     (TK_TYPE_BOOL, "Bool"),
+     (TK_TYPE_INT, "Int"),
+     (TK_TYPE_TEXT, "Text"),
+     (TK_TYPE_ATOM, "Atom"),
+     (TK_TYPE_TUP, "Tup"),
+     (TK_TYPE_REL, "Rel"),
+     (TK_TYPE_FUNC, "Func"),
+     (TK_TYPE_ANY, "Any"),
+     (TK_EQUAL, "="),
+     (TK_ASSIGN, ":="),
+     (TK_PLUS, "+"),
+     (TK_MUL, "*"),
+     (TK_DIV, "/"),
+     (TK_MOD, "mod"),
+     (TK_SEMICOLON, ";"),
+     (TK_CONCAT, "++"),
      ],
     key=lambda p : -len(p[1])
     )
@@ -45,6 +101,8 @@ for k,v in tokenMap:
     mapToken[k] = "\"%s\""%v
 mapToken[TK_EOF] = "End of file"
 mapToken[TK_NAME] = "Name"
+mapToken[TK_INT] = "Int"
+mapToken[TK_TEXT] = "Text"
 
 class Lexer:
 
@@ -76,7 +134,26 @@ class Lexer:
                 j+=1
             self.index += j
             return (TK_NAME, i, j)
-            
+        
+        # check if it is an int
+        if c[i].isdigit():
+            j = 0
+            while i+j < len(c) and c[i+j].isdigit():
+                j+=1
+            self.index += j
+            return (TK_INT, i, j)    
+
+        # check if it is a text
+        if c[i] == '"':
+            j = 1
+            while i + j < len(c) and c[i+j] != '"':
+                j+=1
+            if i+j == len(c):
+                self.index += j
+                return (TK_ERR, i, j)
+            self.index += j+1
+            return (TK_TEXT, i, j+1)
+        
         j = 0
         while i+j < len(c) and c[i+j] not in " \t\r\n":
             j = j + 1
--- parser.py
+++ parser.py
@@ -16,21 +16,82 @@ class Parser:
     def nextToken(self):
         self.currentToken = self.lexer.getNext()
 
+    def parseType(self):
+        cToken = self.currentToken[0]
+        if cToken in [TK_TYPE_BOOL,TK_TYPE_INT,TK_TYPE_TEXT,TK_TYPE_ATOM,TK_TYPE_TUP,TK_TYPE_REL,TK_TYPE_FUNC,TK_TYPE_ANY]:
+            self.nextToken()
+            return
+        raise ParserException("Expected Type", self.currentToken)
+
+    def parseAssignment(self):       
+        self.nextToken()
+        if self.currentToken[0] == TK_ASSIGN:
+            self.nextToken()
+            self.parseExp()
+
     def parseBottomExp(self):
         cToken = self.currentToken[0]
         if False:
             pass
+        elif cToken == TK_MINUS:
+            self.nextToken()
+            self.parseExp()
+        elif cToken == TK_NAME:
+            self.parseAssignment()
+        elif cToken == TK_FUNC:
+            self.nextToken()
+            self.assertToken(TK_LPAREN)
+            if self.currentToken[0] != TK_RPAREN:
+                self.assertToken(TK_NAME)
+                self.assertToken(TK_COLON)
+                self.parseType()
+                while self.currentToken[0] == TK_COMMA:
+                    self.nextToken()
+                    self.assertToken(TK_NAME)
+                    self.assertToken(TK_COLON)
+                    self.parseType()
+            self.assertToken(TK_RPAREN)
+            self.assertToken(TK_RIGHTARROW)
+            self.assertToken(TK_LPAREN)
+            self.parseType()
+            self.assertToken(TK_RPAREN)
+            self.parseExp()
+            self.assertToken(TK_END)
+        elif cToken == TK_REL:
+            self.nextToken()
+            self.assertToken(TK_LPAREN)
+            self.parseExp()
+            self.assertToken(TK_RPAREN)
+        elif cToken == TK_TUP:
+            self.nextToken()
+            self.assertToken(TK_LPAREN)
+            if self.currentToken[0] != TK_RPAREN:
+                self.assertToken(TK_NAME)
+                self.assertToken(TK_COLON)
+                self.parseExp()
+                while self.currentToken[0] == TK_COMMA:
+                    self.nextToken()
+                    self.assertToken(TK_NAME)
+                    self.assertToken(TK_COLON)
+                    self.parseExp()
+                    
+            self.assertToken(TK_RPAREN)
+        elif cToken in [TK_ZERO, TK_ONE, TK_STDBOOL, TK_STDINT, TK_STDTEXT, TK_SHARP]:
+            self.nextToken()
+        elif cToken == TK_TEXT:
+            self.nextToken()
+        elif cToken == TK_INT:
+            self.nextToken()
         elif cToken == TK_BLOCKSTART:
             self.nextToken()
             while self.currentToken[0] == TK_VAL:
                 self.nextToken()
                 self.assertToken(TK_NAME)
-                self.assertToken(TK_COLON)
+                self.assertToken(TK_EQUAL)
                 self.parseExp()
             self.assertToken(TK_IN)
             self.parseExp()
             self.assertToken(TK_BLOCKEND)
-            pass
         elif cToken == TK_AT:
             self.nextToken()
             self.assertToken(TK_LPAREN)
@@ -39,22 +100,55 @@ class Parser:
         elif cToken == TK_NOT:
             self.nextToken()
             self.parseExp()
-        elif cToken == TK_TRUE:
-            self.nextToken()
-        elif cToken == TK_FALSE:
+        elif cToken in [TK_TRUE,TK_FALSE]:
             self.nextToken()
         else:
             raise ParserException("Unexpected token", self.currentToken)
-            
+
+    def parseConcatExp(self):
+        self.parseBottomExp()
+        while self.currentToken[0] == TK_CONCAT:
+            self.nextToken()
+            self.parseBottomExp()
+
+    def parseSequenceExp(self):
+        self.parseOrExp()
+        while self.currentToken[0] == TK_SEMICOLON:
+            self.nextToken()
+            self.parseOrExp()
+
+    def parseAndExp(self):
+        self.parsePlusMinusExp()
+        while self.currentToken[0] == TK_AND:
+            self.nextToken()
+            self.parsePlusMinusExp()
+
+    def parseOrExp(self):
+        self.parseAndExp()
+        while self.currentToken[0] == TK_OR:
+            self.nextToken()
+            self.parseAndExp()
+        
+    def parseMulDivExp(self):
+        self.parseConcatExp()
+        while self.currentToken[0] in [TK_DIV, TK_MUL, TK_MOD]:
+            self.nextToken()
+            self.parseConcatExp()
+
+
+    def parsePlusMinusExp(self):
+        self.parseMulDivExp()
+        while self.currentToken[0] in [TK_PLUS, TK_MINUS]:
+            self.nextToken()
+            self.parseMulDivExp()
+
     def parse(self) :
         try: 
             self.parseExp()
             self.assertToken(TK_EOF)
         except ParserException as e:
             startOfLine = self.code.rfind("\n", 0, e.TK[1])
-            if startOfLine == -1:
-                startOfLine = 0
-
+            startOfLine += 1
             endOfLine = self.code.find("\n", startOfLine+1)
             if endOfLine == -1:
                 endOfLine = len(self.code)
@@ -64,9 +158,8 @@ class Parser:
             print "%s^%s"%(" "*(e.TK[1]-startOfLine), "~"*(e.TK[2]-1))
 
     def parseExp(self):
-        self.parseBottomExp();
+        self.parseSequenceExp()     
         
-
     def assertToken(self, TK):
         if TK != self.currentToken[0]:
             # raise an error
