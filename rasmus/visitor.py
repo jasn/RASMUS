d3378efaf0cf54f82768e8369ca16d08b6b80ef8
Antialize <antialize@gmail.com>
2013-11-09 22:26:29 +0100

    Put some default functions into the visitor, for stuff that you do not alwayes want to visit

--- rasmus/visitor.py
+++ rasmus/visitor.py
@@ -2,6 +2,21 @@ class Visitor:
     def visit(self, node):
         return node.visit(self)
 
+    def visitRenameItem(self, node):
+        pass
+
+    def visitChoice(self, node):
+        pass
+
+    def visitTupItem(self, node):
+        pass
+
+    def visitFuncArg(self, node):
+        pass
+
+    def visitVal(self, node):
+        pass
+
     def visitAll(self, nodes):
         for node in nodes:
             node.visit(self)
