634a48465516716b56f0c55ea32d874039f9b9be
Antialize <antialize@gmail.com>
2013-11-09 22:28:09 +0100

    Fix up RASMUS to also be an rpython target

--- RASMUS.py
+++ RASMUS.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python
-import sys
 import rasmus.lexer
 import rasmus.parser
 import rasmus.error
 import rasmus.jsonPrinter
-import rasmus.llvmCodeGen
+#import rasmus.llvmCodeGen
 import rasmus.charRanges
 import rasmus.firstParse
 
-def main():
-    if len(sys.argv) != 2:
-        exit(1)
+def entry_point(argv):
+    if len(argv) != 2:
+        return 1
     
-    f = sys.argv[1]
-    
-    with open(f) as myFile:
-        code = myFile.read()
-        e = rasmus.error.Error(code, f)
-        p = rasmus.parser.Parser(e, code)
-        AST = p.parse()
-        rasmus.charRanges.CharRanges().visit(AST)
-        rasmus.firstParse.FirstParse(e, code).visit(AST)
-        #rasmus.llvmCodeGen.LLVMCodeGen(code, sys.stdout).generate(AST)
-        rasmus.jsonPrinter.JSONPrinter(code, open("ast.json","w")).visit(AST)
-        sys.stdout.write("\n")
+    f = argv[1]
+    myFile = open(f)
+    code = myFile.read()
+    e = rasmus.error.Error(code, f)
+    p = rasmus.parser.Parser(e, code)
+    AST = p.parse()
+    rasmus.charRanges.CharRanges().visit(AST)
+    rasmus.firstParse.FirstParse(e, code).visit(AST)
+    rasmus.jsonPrinter.JSONPrinter(code, open("ast.json","w")).visit(AST)
+
+    #rasmus.llvmCodeGen.LLVMCodeGen(code, sys.stdout).generate(AST)
+    #sys.stdout.write("\n")
+    return 0
+
+def target(*args):
+    return entry_point, None
 
 if __name__ == "__main__":
-    main()
+    import sys
+    entry_point(sys.argv)
+
