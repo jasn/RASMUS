77f9cc8bb1c580d6b7213b20d35199518a409d16
Antialize <antialize@gmail.com>
2013-11-10 14:37:03 +0100

    Add partial repl for RASMUS

--- RASMUS.py
+++ RASMUS.py
@@ -6,24 +6,71 @@ import rasmus.jsonPrinter
 #import rasmus.llvmCodeGen
 import rasmus.charRanges
 import rasmus.firstParse
+import rasmus.code
+import rasmus.AST
+import os
 
-def entry_point(argv):
-    if len(argv) != 2:
-        return 1
-    
-    f = argv[1]
-    myFile = open(f)
-    code = myFile.read()
-    e = rasmus.error.Error(code, f)
+def run_file(path):
+    myFile = open(path)
+    code = rasmus.code.Code(myFile.read(), "hat")
+    e = rasmus.error.Error(code)
     p = rasmus.parser.Parser(e, code)
     AST = p.parse()
     rasmus.charRanges.CharRanges().visit(AST)
     rasmus.firstParse.FirstParse(e, code).visit(AST)
-    rasmus.jsonPrinter.JSONPrinter(code, open("ast.json","w")).visit(AST)
-
-    #rasmus.llvmCodeGen.LLVMCodeGen(code, sys.stdout).generate(AST)
-    #sys.stdout.write("\n")
     return 0
+
+def hat(hat):
+    return True
+
+def run_terminal():
+    code = rasmus.code.Code("", "Interpreted")
+    e = rasmus.error.Error(code)
+    typeChecker = rasmus.firstParse.FirstParse(e, code)
+    outerLus = [{}]
+    theCode = ""
+    incomplete = ""
+    sequenceExpNode = rasmus.AST.SequenceExp()    
+    while True:       
+        if incomplete:
+            os.write(0, ".... ")
+        else:
+            os.write(0, ">>>> ")
+        line = os.read(2, 1024*10)
+        if len(line) == 0: break
+        newCode = theCode + incomplete + line
+        code.setCode(newCode)
+        p = rasmus.parser.Parser(e, code, True, len(theCode))
+        try:
+            errorsPrior = e.numberOfErrors
+            AST = p.parse()
+            if not isinstance(AST, rasmus.AST.InvalidExp):
+                AST=AST.exp
+                rasmus.charRanges.CharRanges().visit(AST)
+                sequenceExpNode.sequence.append(AST)
+                typeChecker.setLus(outerLus)
+                typeChecker.visit(AST)
+                errorsNow = e.numberOfErrors
+                if errorsNow == errorsPrior:
+                    outerLus = typeChecker.getLus()
+                    theCode = newCode + ";\n"
+                    #bytecode = codegen.visit(AST)
+                    #interperter.run(bytecode)
+                else:
+                    sequenceExpNode.sequence.pop()
+                incomplete = ""
+        except rasmus.parser.IncompleteInputException:
+            incomplete += line + "\n"    
+    return 0
+
+def entry_point(argv):
+    if len(argv) == 1:
+        return run_terminal()
+
+    if len(argv) == 2:
+        return run_file(argv[1])
+
+    return 1
 
 def target(*args):
     return entry_point, None
