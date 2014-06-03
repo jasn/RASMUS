c4dcdd3845366177400cfb0c988f2db05708ea6e
Jakob Truelsen <jakob@scalgo.com>
2014-06-03 10:54:33 +0200

    Make int-speed-test less over-optimizable by using std::shuffle, now it seems that doubels are more then 3 times faster

--- test/int-speed-test.cc
+++ test/int-speed-test.cc
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <algorithm>
+#include <random>
+using namespace std;
 using namespace std::chrono;
 
 class OInt {
@@ -36,17 +39,23 @@ void test(std::string name, IT undef) {
   for (size_t i=0; i < value_count; ++i)
     values[i] = IT(i);
   
+
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  std::default_random_engine rng(seed);
   IT sum1(0);
   IT sum2(0);
   high_resolution_clock::time_point t1 = high_resolution_clock::now();
   {
-    for (size_t i = 0; i < rep_count; ++i)
+    shuffle(begin(values), end(values), rng);
+    for (size_t i = 0; i < rep_count; ++i) {
       for (IT x: values)
 	sum1 = sum1 + x;
+    }
   }
   high_resolution_clock::time_point t2 = high_resolution_clock::now();
   {
     values[42] = undef;
+    shuffle(begin(values), end(values), rng);
     for (size_t i = 0; i < rep_count; ++i)
       for (IT x: values)
 	sum2 = sum2 + x;
