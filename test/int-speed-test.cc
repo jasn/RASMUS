90543951ae780db06761450a2270a9635f95de63
Jakob Truelsen <jakob@scalgo.com>
2014-06-03 10:50:56 +0200

    Add test of different wayes of implementing ints with ?-Int
    Curretly we only test addition. There are two implementations one that uses two cmoves and ints
    and another that uses doubles and nan. Doubles seems to win but not by a lot

---
+++ test/int-speed-test.cc
@@ -0,0 +1,66 @@
+#include <cstdint>
+#include <limits>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <chrono>
+using namespace std::chrono;
+
+class OInt {
+public:
+
+  static const int64_t udef=std::numeric_limits<int64_t>::min();
+  int64_t value;
+  OInt() {}
+  OInt(int64_t value): value(value) {}
+ 
+  friend OInt operator+(OInt a, OInt b) {
+    int64_t v = a.value + b.value;
+    if (a.value == udef) v=udef;
+    if (b.value == udef) v=udef;
+    return OInt(v);
+  }
+  friend std::ostream & operator<<(std::ostream & o, OInt v) {
+    if (v.value == udef) return o << "?-Int";
+    return o << v.value;
+  }
+};
+
+double undef() {return std::numeric_limits<double>::quiet_NaN();}
+
+template <typename IT>
+void test(std::string name, IT undef) {
+  const size_t value_count=500000;
+  const size_t rep_count=2000;
+  IT values[value_count];
+  for (size_t i=0; i < value_count; ++i)
+    values[i] = IT(i);
+  
+  IT sum1(0);
+  IT sum2(0);
+  high_resolution_clock::time_point t1 = high_resolution_clock::now();
+  {
+    for (size_t i = 0; i < rep_count; ++i)
+      for (IT x: values)
+	sum1 = sum1 + x;
+  }
+  high_resolution_clock::time_point t2 = high_resolution_clock::now();
+  {
+    values[42] = undef;
+    for (size_t i = 0; i < rep_count; ++i)
+      for (IT x: values)
+	sum2 = sum2 + x;
+  }
+  high_resolution_clock::time_point t3 = high_resolution_clock::now();
+
+  duration<double> d1 = duration_cast<duration<double>>(t2 - t1);
+  duration<double> d2 = duration_cast<duration<double>>(t3 - t2);
+
+  std::cout << name << " " << d1.count() << " " << d2.count() << " " << sum1 << " " << sum2 << std::endl;
+}
+
+int main() {
+  test<double>("double", std::numeric_limits<double>::quiet_NaN());
+  test<OInt>("cmove", OInt::udef);
+}
+    
