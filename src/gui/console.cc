6d6b8f201ec52c37e6d51a1b7106dd883b51bb8c
Jesper Sindahl Nielsen <jesper.sindahl.nielsen@gmail.com>
2014-08-11 21:59:18 +0200

    Implemented history functionality in gui. Fixes #21.

--- src/gui/console.cc
+++ src/gui/console.cc
@@ -1,8 +1,35 @@
 #include <console.hh>
 #include <QTextBlock>
 #include <iostream>
+#include <vector>
+
+
+void Console::updateHistory() {
+  QTextCursor c = textCursor();
+
+  QString currentLine = c.document()->lastBlock().text().right(c.document()->lastBlock().text().size() -4);
+  
+  history[history.size()-1-currHistoryPosition] = currentLine;
+
+}
+
+// update last line of last line in gui to show history[currentHistoryPosition]
+void Console::rewriteCurrentLine() {
+  QTextCursor c = textCursor();
+  
+  int firstInLastBlock = c.document()->lastBlock().position()+4;
+  int lastInLastBlock = firstInLastBlock + c.document()->lastBlock().text().size() - 4;
+  c.setPosition(firstInLastBlock);
+  c.setPosition(lastInLastBlock, QTextCursor::KeepAnchor);
+  c.removeSelectedText();
+
+  c.insertText(history[history.size()-currHistoryPosition-1]);
+  ensureCursorVisible();
+
+}
 
 void Console::keyPressEvent(QKeyEvent *e) {
+  
     QTextCursor c = textCursor();
     
     int firstInLastBlock = c.document()->lastBlock().position() + 4;
@@ -12,6 +39,9 @@ void Console::keyPressEvent(QKeyEvent *e) {
       c.movePosition(QTextCursor::End);
       setTextCursor(c);
     }
+
+    if (history.size() == 0) history.push_back(QString::fromStdString(""));
+
     switch (e->key()) {
     case Qt::Key_Backspace:
       if (c.positionInBlock() > 4) 
@@ -20,26 +50,41 @@ void Console::keyPressEvent(QKeyEvent *e) {
     case Qt::Key_Left:
       if (c.positionInBlock() > 4)
 	QPlainTextEdit::keyPressEvent(e);
-      break;
-	
+      break;	
     case Qt::Key_Right:
       QPlainTextEdit::keyPressEvent(e);
       break;
     case Qt::Key_Up:
+      if (history.size()-1 == currHistoryPosition) break;
+      ++currHistoryPosition;
+      rewriteCurrentLine();
+      break;
     case Qt::Key_Down:
+      if (currHistoryPosition == 0) break;
+      --currHistoryPosition;
+      rewriteCurrentLine();
       break;
     case Qt::Key_Enter:
     case Qt::Key_Return:
       if (c.document()->lastBlock().text().size() <= 4) {
 	c.movePosition(QTextCursor::End);
-	c.insertText("\n");
-      } else
-	emit run(c.document()->lastBlock().text().right(c.document()->lastBlock().text().size() -4));
+	insertEmptyBlock();
+      } else {
+	QString tmp = c.document()->lastBlock().text().right(c.document()->lastBlock().text().size() -4);
+	emit run(tmp);
+	currHistoryPosition = 0;
+	history[history.size()-1] = tmp;
+	history.push_back(QString::fromStdString(""));
+	
+      }
+
       break;
       
     default:
       QPlainTextEdit::keyPressEvent(e);
+      updateHistory();
     }
+
 }
 
 void Console::incomplete() {
@@ -57,6 +102,10 @@ void Console::incomplete() {
 }
 
 void Console::complete() {
+  insertEmptyBlock();
+}
+
+void Console::insertEmptyBlock() {
   QTextCursor c = textCursor();
   c.movePosition(QTextCursor::End);
   c.insertBlock();
@@ -77,5 +126,5 @@ void Console::display(QString block) {
   ensureCursorVisible();
 }
 
-Console::Console(QWidget * parent): QPlainTextEdit(parent) {
+Console::Console(QWidget * parent): QPlainTextEdit(parent), history(std::vector<QString>()), currHistoryPosition(0), currentLineInsertedInHistory(false) {
 }
